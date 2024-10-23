3becc395cad1c9d714c05746c01178a7dfbe5872
lucas.franchi <lucas.franchi12@outlook.com>
2024-10-23 19:18:22 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'relatorios',
+  webDir: 'www'
+};
+
+export default config;
